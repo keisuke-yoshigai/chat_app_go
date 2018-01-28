63eed9234e5b1a98754d7e95c56348e3683f909a
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2018-01-28 12:13:50 +0900

    テンプレートファイル作成

--- main.go
+++ main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func Index(w http.ResponseWriter, req *http.Request) {
-    html := `
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>チャット</title>
-        </head>
-        <body>
-            <h1>チャットを始めよう</h1>
-        </body>
-    </html>
-    `
-    w.Write([]byte(html))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	t.once.Do(func() { //once.Do(f)は初めての呼び出しのみ実行されるメソッド
+		t.temp1 =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	if err := t.temp1.Execute(w, nil); err != nil { //temlateオブジェクトをio.Writerに書き込む
+		log.Fatal(err)
+	}
 }
 
 func main() {
-    http.HandleFunc("/", Index)
-    if err := http.ListenAndServe(":8090", nil); err != nil {
-        log.Fatal(err)
-    }
+	ptTmpHandler := templateHandler{filename: "chat.html"}
+	http.Handle("/", &ptTmpHandler)
+	if err := http.ListenAndServe(":8090", nil); err != nil {
+		log.Fatal(err)
+	}
 }
